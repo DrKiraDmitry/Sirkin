1b4d04ea71236e923da79800820ab440f5eff59e
Dima <stalker2807sky@yandex.ru>
2019-06-20 03:51:10 +0700

    Lab1.3 Lab1.5

---
+++ LabSir1/LabSir1.3/LabSir1.3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LabSir1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                for (int z = 0; z < 1000; z++)
+                {
+                    if (i == z)
+                    {
+                        Console.WriteLine(i+ "=" +z);
+                    }
+                    else
+                    {
+                        int c1 = i / 100;
+                        int c2 = i / 10;
+                         c2 = c2 % 10;
+                        int c3 = i % 10;
+                        int c4 = c1 + c2 + c3;
+                        int f1 = z / 100;
+                        int f2 = z / 10;
+                        f2 = f2 % 10;
+                        int f3 = z % 10;
+                        int f4 = f1 + f2 + f3;
+                        if (f4 == c4)
+                        {
+                            Console.WriteLine(i + "=" + z );
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ LabSir1/LabSir1.5/LabSir1.5/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace LabSir1._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Позиция по оси X =");
+            string x = Console.ReadLine();
+            int xf = Convert.ToInt32(x);
+            if (xf > 8)
+            {
+                Console.WriteLine("Позиция по оси X не может быть больше 8");
+            }
+            else
+                if (xf < 1)
+            {
+                Console.WriteLine("Позиция по оси X не может быть меньше 1");
+            };
+            Console.WriteLine("Позиция по оси Y =");
+            string y = Console.ReadLine();
+            int yf = Convert.ToInt32(y);
+            if (yf > 8)
+            {
+                Console.WriteLine("Позиция по оси Y не может быть больше 8");
+            }
+            else
+                if (yf < 1)
+            {
+                Console.WriteLine("Позиция по оси Y не может быть меньше 1");
+            };
+            for (int yb=1; yb<9; yb+=1)
+            {
+                for (int xb = 1; xb < 9; xb += 1)
+                {
+                    
+                    int y1 = yf + 2;
+                    int x1 = xf - 1;
+                    if (y1 == yb && x1 == xb)
+                    {
+                        Console.WriteLine("Y"+yb + " " + "X" + xb);
+                    };
+                    //
+                    //Top
+                    //
+                    int y2 = yf + 2;
+                    int x2 = xf + 1;
+                    if (y2 == yb && x2 == xb)
+                    {
+                        Console.WriteLine("Y"+yb + " " + "X" + xb);
+                    };
+
+                    int y3 = yf + 1;
+                    int x3 = xf + 2;
+                    if (y3 == yb && x3 == xb)
+                    {
+                        Console.WriteLine("Y"+yb + " " + "X" + xb);
+                    };
+                    //
+                    //Rigth
+                    //
+                    int y4 = yf - 1;
+                    int x4 = xf + 2;
+                    if (y4 == yb && x4 == xb)
+                    {
+                        Console.WriteLine("Y"+yb + " " + "X" + xb);
+                    };
+
+                    int y5 = yf - 2;
+                    int x5 = xf + 1;
+                    if (y5 == yb && x5 == xb)
+                    {
+                        Console.WriteLine("Y"+yb + " " + "X" + xb);
+                    };
+                    //
+                    //Bot
+                    //
+                    int y6 = yf - 2;
+                    int x6 = xf - 1;
+                    if (y6 == yb && x6 == xb)
+                    {
+                        Console.WriteLine("Y"+yb + " " + "X" + xb);
+                    };
+
+                    int y7 = yf - 1;
+                    int x7 = xf - 2;
+                    if (y7 == yb && x7 == xb)
+                    {
+                        Console.WriteLine("Y"+yb + " " + "X" + xb);
+                    };
+                    //
+                    //Left
+                    //
+                    int y8 = yf + 1;
+                    int x8 = xf - 2;
+                    if (y8 == yb && x8 == xb)
+                    {
+                        Console.WriteLine("Y"+yb + " " + "X" + xb);
+                    };
+                };
+            };
+        }
+    }
+}
